3892aa9fa689a7a8b5e31c32ba3a5fedc9929f47
ikeikeikeike <jp.ne.co.jp@gmail.com>
2012-12-31 11:09:57 +0900

    Available unittest

--- queryset_client/tests/base/tests/__init__.py
+++ queryset_client/tests/base/tests/__init__.py
@@ -1,17 +1,17 @@
 import warnings
 warnings.simplefilter('ignore', Warning)
 
-# from .count import *
-# from .create import *
-# from .exists import *
-# from .foreignkey import *
-# from .get_or_create import *
-# from .latest import *
-# from .lazy import *
-# from .len import *
-# from .many_to_many import *
-# from .model import *
-# from .order_by import *
-# from .paginator import *
-# from .slice import *
+from .count import *
+from .create import *
+from .exists import *
+from .foreignkey import *
+from .get_or_create import *
+from .latest import *
+from .lazy import *
+from .len import *
+from .many_to_many import *
+from .model import *
+from .order_by import *
+from .paginator import *
+from .slice import *
 from .strict import *
