d16c3198abf1145452325563c27b9e5616f202a9
ikeikeikeike <jp.ne.co.jp@gmail.com>
2013-02-17 07:51:09 +0900

    Fix ver 0.7.2

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     data_files=data_files,
-    install_requires=["slumber", "requests"],
+    install_requires=["slumber", "simplejson", "requests"],
     # cmdclass={'test': pytest_test},
     # tests_require=['pytest'],
 )
